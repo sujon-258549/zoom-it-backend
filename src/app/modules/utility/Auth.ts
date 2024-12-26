50b8647c9c99d0a96445d351fc79a0ce89b4eb8d
Md Sujon Mia <sujon258549@gmail.com>
2024-12-26 22:05:13 +0600

    modified Auth.ts in utility

--- src/app/modules/utility/Auth.ts
+++ src/app/modules/utility/Auth.ts
@@ -1,14 +1,16 @@
+import jwt from 'jsonwebtoken';
 import { NextFunction, Request, Response } from 'express';
 import { TuserRole } from '../User/User.interface';
 import { catchAsynch } from './catchAsync';
+import config from '../../config';
 
 const auth = (...requerdRole: TuserRole[]) => {
   return catchAsynch(
     async (req: Request, res: Response, next: NextFunction) => {
       const token = req.headers.authorization;
-      console.log(token, requerdRole);
 
       //   validaction token
+      const decoded = jwt.verify(token, config.JWT_ACCESS_TOCEN);
       next();
     },
   );
