5be593cb22d74395b42662bd965eeec365bd46b9
Md Sujon Mia <sujon258549@gmail.com>
2024-12-27 01:34:08 +0600

    modified Blog.Servises.ts in Blog

--- src/app/modules/Blog/Blog.Servises.ts
+++ src/app/modules/Blog/Blog.Servises.ts
@@ -1,3 +1,4 @@
+import { Types } from 'mongoose';
 import { User } from '../User/User.mole';
 import { TBlog } from './Blog.interfaces';
 import { Blog } from './Blog.Model';
@@ -24,27 +25,35 @@ const updateBlogintoDB = async (id: string, paylod: Partial<TBlog>) => {
   const result = await Blog.findByIdAndUpdate(id, paylod);
   return result;
 };
-// delete bloge
 const deleteBlogintoDB = async (authorId: string, email: string) => {
-  // Compare userId with the blog's author
-  const blog = await Blog.findOne({ authorId });
-  console.log(authorId);
+  // Fetch the user's ObjectId by email
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('User not found.');
+  }
+
+  // Get the userId from the user object
+  const userId = user._id.toString();
+
+  // Find the blog by authorId
+  const blog = await Blog.findOne(new Types.ObjectId(authorId));
   if (!blog) {
     throw new Error('Blog not found.');
   }
 
-  // Fetch the user's ObjectId
-  const userId = await User.isUserExistsById(email);
-  if (!userId) {
-    throw new Error('User not found.');
-  }
+  console.log(blog, authorId, user);
 
-  if (blog.author.toString() !== userId.toString()) {
+  // Check if the user is the author of the blog
+  if (userId !== blog.author.toString()) {
     throw new Error('Unauthorized access. You can only delete your own blogs.');
   }
 
   // Delete the blog
-  const result = await Blog.findOneAndDelete({ authorId });
+  const result = await Blog.findOneAndDelete({ new Types.ObjectId(authorId)});
+  if (!result) {
+    throw new Error('Blog not found or already deleted.');
+  }
+
   return result;
 };
 
